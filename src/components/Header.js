bda9736a0978ba82cdb54c78de37c83b3528e62c
Piyush Prajapati <piyushoa2004@gmail.com>
2023-08-31 23:06:07 +0530

    alpha-6

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,46 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 
+// const THEME_SYSTEM = -1;
+// const THEME_LIGHT = 0;
+// const THEME_DARK = 1;
+
 export default function Header() {
 
-    const [theme, setTheme] = useState('light');
+    // const [theme, setTheme] = useState();
+
+    let isSystemDark = window.matchMedia("(prefers-color-scheme: dark)");
+    let abc = isSystemDark.matches;
+    const initialTheme = abc ? 'dark' : 'light';
+    const [theme, setTheme] = useState(initialTheme);
+
+    isSystemDark.addEventListener('change',() => {
+        const newIsSystemDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        if (newIsSystemDark !== isSystemDark.matches) abc = newIsSystemDark;
+    });
+
+    const finalTheme = isSystemDark.matches && localStorage.getItem('darkMode');
+
+    console.log(finalTheme);
 
     useEffect(() => {
-        const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-        console.log(prefersDarkMode);
-        setTheme(prefersDarkMode ? 'dark' : 'light');
+        setTheme(finalTheme ? 'dark' : 'light');
     }, []);
 
     useEffect(() => {
+        isSystemDark.matches ? setTheme('dark') : setTheme('light');
+    }, [isSystemDark.matches]);
+
+    useEffect(() => {
         if (theme === 'dark') {
-            document.querySelector('html').classList.add('dark');
-            localStorage.setItem('theme', 'dark');
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('darkMode', true);
         } else {
-            document.querySelector('html').classList.remove('dark');
-            localStorage.setItem('theme', 'light');
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('darkMode', false);
         }
     }, [theme]);
+
 
     const themeToggle = () => {
         setTheme(theme === 'light' ? 'dark' : 'light')
@@ -29,6 +50,16 @@ export default function Header() {
         <div className='flex justify-between mb-3'>
             <p className='font-bold'>Calc</p>
             <div className='flex items-center'>
+                <i className="fa-solid fa-sun text-[20px] text-[#f1bf40]"></i>
+                <label className="switch">
+                    <input
+                        type="checkbox"
+                        className='switch-input'
+                        checked={theme === 'dark'}
+                        onChange={themeToggle}
+                    />
+                    <span className="slider bg-[#b8b8b8] dark:bg-keypad-back-dark"></span>
+                </label>
                 <i className="fa-solid fa-sun text-[20px] text-[#f1bf40]"></i>
                 <label className="switch">
                     <input
