8f5be465092c6e693b9ac5a7d0d3a1d8213dd8d5
Piyush Prajapati <piyushoa2004@gmail.com>
2023-08-31 23:06:31 +0530

    alpha-6

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -41,7 +41,7 @@ export default function Keypad() {
             let e = element.replaceAll("√", " √");
             let a = e.trim().split(" ");
             a.forEach((e, i) => {
-                if (e.charAt(0) == "√")
+                if (e.charAt(0) === "√")
                     a[i] = parseFloat(Math.sqrt(e.slice(1)));
             });
             let elementResult = a.reduce((a, b) => a * b, 1);
@@ -107,74 +107,40 @@ export default function Keypad() {
         }
 
         if (stack.length > 1) {
-            return "Invali";
+            return "Invalid";
         } else {
             return stack[0];
         }
     }
 
-    // const clickHandler = (value) => {
-    //     if(expression === 'Invalid Expression') {
-    //         setExpression(0);
-    //     }
-    //     else if (value === 'AC') {
-    //         setExpression(0);
-    //     }
-    //     else if (value === '⌫') {
-    //         if (expression.length > 1) {
-    //             setExpression(expression.slice(0, -1));
-    //         } else {
-    //             setExpression(0)
-    //         }
-    //     }
-    //     else if (value === '=') {
-    //         try {
-    //             let result = eval(expression);
-    //             setExpression(result);
-    //         } catch (error) {
-    //             setExpression('Invalid Expression');
-    //         }
-    //     }
-    //     else {
-    //         if (expression === 0) {
-    //             setExpression(value);
-    //         } else {
-    //             if (!(expression.charAt(expression.length - 1).match(/^[-+×÷%.√]+$/) && value.match(/^[-+×÷%.√]+$/))) {
-    //                 setExpression(expression + value);
-    //             }
-    //         }
-    //     }
-    // }
-
     return (
         <>
             <div className='bg-screen-back-light dark:bg-screen-back-dark py-1 px-2 rounded-md text-right mb-3 h-[100px]'>
                 {result && <p className=' text-[16px] overflow-hidden'>{expression}</p>}
-                <p className=' text-[32px] overflow-hidden'>{result ? result : expression}</p>
+                <p className='font-bold text-[32px] overflow-hidden'>{result ? result : expression}</p>
             </div>
-            <div className='bg-keypad-back-light dark:bg-keypad-back-dark grid grid-cols-4 rounded-md p-1'>
-
-                <ImpKeys value={'AC'} clickHandler={onClear} />
-                <Keys value={'√'} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={'÷'} clickHandler={(value) => onValueInput(value)} />
-                <ImpKeys value={'⌫'} clickHandler={onBackspace} />
-                <Keys value={7} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={8} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={9} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={'×'} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={4} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={5} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={6} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={'-'} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={1} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={2} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={3} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={'+'} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={'.'} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={0} clickHandler={(value) => onValueInput(value)} />
-                <Keys value={'%'} clickHandler={(value) => onValueInput(value)} />
-                <ImpKeys value={'='} clickHandler={onResult} />
-
+            <div className='bg-keypad-back-light dark:bg-keypad-back-dark rounded-md p-1'>
+                <div className='grid grid-cols-4'>
+                    <ImpKeys value={'AC'} clickHandler={onClear} />
+                    <Keys value={'√'} clickHandler={(value) => onValueInput(value)} />
+                    <Keys value={'÷'} clickHandler={(value) => onValueInput(value)} />
+                    <ImpKeys value={'⌫'} clickHandler={onBackspace} />
+                    <Keys value={7} clickHandler={(value) => onValueInput(value)} />
+                    <Keys value={8} clickHandler={(value) => onValueInput(value)} />
+                    <Keys value={9} clickHandler={(value) => onValueInput(value)} />
+                    <Keys value={'×'} clickHandler={(value) => onValueInput(value)} />
+                    <Keys value={4} clickHandler={(value) => onValueInput(value)} />
+                    <Keys value={5} clickHandler={(value) => onValueInput(value)} />
+                    <Keys value={6} clickHandler={(value) => onValueInput(value)} />
+                    <Keys value={'-'} clickHandler={(value) => onValueInput(value)} />
+                    <Keys value={1} clickHandler={(value) => onValueInput(value)} />
+                    <Keys value={2} clickHandler={(value) => onValueInput(value)} />
+                    <Keys value={3} clickHandler={(value) => onValueInput(value)} />
+                    <Keys value={'+'} clickHandler={(value) => onValueInput(value)} />
+                    <Keys value={'.'} clickHandler={(value) => onValueInput(value)} />
+                    <Keys value={0} clickHandler={(value) => onValueInput(value)} />
+                    <ImpKeys value={'='} clickHandler={onResult} width />
+                </div>
             </div>
         </>
     )
@@ -191,8 +157,10 @@ const Keys = (props) => {
 
 const ImpKeys = (props) => {
 
+    const width = props.width ? 'col-span-2	' : '';
+
     return (
-        <button className={`text-white bg-imp-back-light dark:bg-imp-back-dark shadow-blue-light dark:shadow-blue-dark hover:bg-imp-hover-light dark:hover:bg-imp-hover-dark h-[60px] flex items-center text-center justify-center rounded-md m-[6px] duration-200 hover:cursor-pointer`} value={props.value} onClick={(e) => props.clickHandler(e.target.value)}>
+        <button className={`text-white bg-imp-back-light dark:bg-imp-back-dark shadow-blue-light dark:shadow-blue-dark hover:bg-imp-hover-light dark:hover:bg-imp-hover-dark h-[60px] ${width} flex items-center text-center justify-center rounded-md m-[6px] duration-200 hover:cursor-pointer `} value={props.value} onClick={(e) => props.clickHandler(e.target.value)}>
             {props.value}
         </button>
     )
